9fc2e746f3c1b2e33928c7b46da8a754f976742a
fidelisaboke <fidelisaboke@gmail.com>
2024-02-14 17:22:29 +0300

    Feat: Add functionality to BudgetMenu, FinRecordsMenu

--- src/com/fidelisaboke/budgetron/BudgetMenu.java
+++ src/com/fidelisaboke/budgetron/BudgetMenu.java
@@ -1,11 +1,13 @@
 package com.fidelisaboke.budgetron;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
 
 public class BudgetMenu {
     private static BudgetMenu instance;
     private final String className = BudgetMenu.class.getName();
+    public static boolean inBudgetMenu;
     private BudgetMenu(){
 
     }
@@ -18,9 +20,16 @@ public class BudgetMenu {
     }
 
     public void start(){
+        inBudgetMenu = true;
+        while(inBudgetMenu){
+            executeBudgetMenu();
+        }
+    }
+
+    private void executeBudgetMenu(){
         String input = JOptionPane.showInputDialog(BudgetronFrame.getInstance(), """
                 Manage Budget
-                1. Display budgets
+                1. Get budgets
                 2. Add budget
                 3. Update budget
                 4. Delete budget
@@ -28,7 +37,7 @@ public class BudgetMenu {
 
         switch(input){
             case "1"->{
-                System.out.println("Display budgets");
+                getBudgets();
             }
             case "2"->{
                 System.out.println("Add budget");
@@ -43,6 +52,7 @@ public class BudgetMenu {
             }
 
             case "0"->{
+                inBudgetMenu = false;
                 BudgetronFrame.inMainMenu = true;
                 BudgetronFrame.getInstance().start();
             }
@@ -57,4 +67,60 @@ public class BudgetMenu {
 
         }
     }
+
+    private void getBudgets(){
+        String input = JOptionPane.showInputDialog(
+                BudgetronFrame.getInstance(),
+                """
+                Get Budgets
+                1. All budgets
+                2. Enter budget to get...
+                0. Back""",
+                "Display Budgets",
+                JOptionPane.PLAIN_MESSAGE);
+
+        switch (input){
+            case "1"->{
+                displayAllBudgets();
+            }
+            case "2"->{
+                String budgetName = JOptionPane.showInputDialog(
+                        BudgetronFrame.getInstance(),
+                        """
+                        Enter budget name:""",
+                        "Enter budget name",
+                        JOptionPane.PLAIN_MESSAGE
+                );
+
+                BudgetRow budgetRow = Budget.getInstance().get("name", budgetName);
+                String budgetRecord = budgetRow.getInfo();
+
+                JOptionPane.showMessageDialog(
+                        BudgetronFrame.getInstance(),
+                        budgetRecord,
+                        "Budget Info",
+                        JOptionPane.PLAIN_MESSAGE
+                );
+            }
+            case "0"->{
+
+            }
+        }
+    }
+
+    // Function that displays all budgets on a message dialog
+    private void displayAllBudgets(){
+        StringBuilder budgetData = new StringBuilder();
+        ArrayList<BudgetRow> budgetRows = Budget.getInstance().getAll();
+        for(BudgetRow budgetRow: budgetRows){
+            budgetData.append(budgetRow.getInfo()).append("\n\n");
+        }
+        JOptionPane.showMessageDialog(
+                BudgetronFrame.getInstance(),
+                budgetData,
+                "All Budgets",
+                JOptionPane.PLAIN_MESSAGE
+        );
+    }
+
 }
--- src/com/fidelisaboke/budgetron/FinRecordsMenu.java
+++ src/com/fidelisaboke/budgetron/FinRecordsMenu.java
@@ -1,5 +1,8 @@
 package com.fidelisaboke.budgetron;
 
+import javax.swing.*;
+import java.util.logging.Level;
+
 public class FinRecordsMenu {
     private static FinRecordsMenu instance;
     private final String className = FinRecordsMenu.class.getName();
@@ -16,7 +19,23 @@ public class FinRecordsMenu {
     }
 
     public void start(){
+        String input = JOptionPane.showInputDialog(BudgetronFrame.getInstance(), """
+               Financial Records Menu
+               1. Personal financial records
+               2. Business financial records
+               """);
 
+        switch (input){
+            case "1"->{
+                System.out.println("Personal financial records");
+            }
+            case "2"->{
+                System.out.println("Business financial records");
+            }
+            default->{
+                MsgHandler.displayMessage("Invalid Option", "Please select a valid option.", className, Level.SEVERE);
+            }
+        }
     }
 
 }
