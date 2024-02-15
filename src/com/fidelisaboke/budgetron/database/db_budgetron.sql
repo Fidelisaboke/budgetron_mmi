f330a563b3448832b26b6ceb4de6afe4bbd43622
fidelisaboke <fidelisaboke@gmail.com>
2024-02-15 10:32:15 +0300

    Move MySQL database file to database package

--- src/com/fidelisaboke/budgetron/database/db_budgetron.sql
+++ src/com/fidelisaboke/budgetron/database/db_budgetron.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2024 at 09:41 AM
+-- Generation Time: Feb 15, 2024 at 08:30 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
